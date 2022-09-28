e36dd65197606302db0f06876c2d97bb6cb54338
geoff-raymann <odiwuorgeoff@gmail.com>
2022-09-28 11:37:41 +0300

    Make Changes

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {
-        'posts':posts
-    })
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {
+#         'page':page,
+#         'posts':posts
+#     })
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by= 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
